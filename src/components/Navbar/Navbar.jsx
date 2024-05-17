0608482bea9258b3bd8bcfc60ea8df1d000e8356
p-k-pavan <pkpavan2003@email.com>
2024-05-17 07:54:46 +0530

    Basic setup

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.css'
+import { assets } from '../../assets/frontend_assets/assets'
+
+const Navbar = () => {
+  return (
+    <div>
+      Navbar
+      
+    </div>
+  )
+}
+
+export default Navbar
