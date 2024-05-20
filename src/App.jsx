4c565e8dcebeff4f7084a287f0805736e3a13264
p-k-pavan <pkpavan2003@email.com>
2024-05-20 08:21:18 +0530

    Path set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import PlaceOrder from './pages/Placeorder/PlaceOrder.jsx'
+import Home from './pages/Home/Home.jsx'
+import Cart from './pages/Cart/Cart.jsx'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <div className="App">
-        
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/cart' element={<Cart />}/>
+          <Route path='/order' element={<PlaceOrder />}/>
+        </Routes>
       </div>
     </div>
   )
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import './Home.css'
+import './Cart.css'
 
-const Home = () => {
+const Cart = () => {
   return (
     <div>
       
@@ -9,4 +9,4 @@ const Home = () => {
   )
 }
 
-export default Home
+export default Cart
