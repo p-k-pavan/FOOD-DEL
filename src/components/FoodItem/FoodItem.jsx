c189bfd77a35dfaa8a5fed11d24d6cb01d5991cb
p-k-pavan <pkpavan2003@email.com>
2024-05-21 21:14:32 +0530

    item count

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./FoodItem.css";
 import { assets } from "../../assets/frontend_assets/assets";
 const FoodItem = ({ id, name, price, description, image }) => {
+  const [itemCount, setItemCount] = useState(0);
+
   return (
     <div className="food-item">
       <div className="food-item-img-continer">
         <img className="food-item-img" src={image} alt="" />
+        {!itemCount ? (
+          <img
+            src={assets.add_icon_white}
+            onClick={() => setItemCount((prev) => prev + 1)}
+            alt=""
+            className="add"
+          />
+        ) : (
+          <div className="food-item-counter">
+            <img
+              onClick={() => setItemCount((prev) => prev - 1)}
+              src={assets.remove_icon_red}
+              alt=""
+            />
+            <p>{itemCount}</p>
+            <img
+              onClick={() => setItemCount((prev) => prev + 1)}
+              src={assets.add_icon_green}
+              alt=""
+            />
+          </div>
+        )}
       </div>
       <div className="food-item-info">
         <div className="food-item-name-rating">
