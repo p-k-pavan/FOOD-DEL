c9a51cef06e5b49753eded7abdffeda54db8367a
p-k-pavan <pkpavan2003@email.com>
2024-07-12 13:25:43 +0530

    Create Admin Panel using React JS

---
+++ admin/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import Sidebar from './components/Sidebar/Sidebar';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import ADD from './pages/ADD/ADD';
+import List from './pages/List/List';
+import Order from './pages/Orders/Order';
+import './App.css'; // Adding a new CSS file for layout styles
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const App = () => {
+  return (
+    <div>
+      <ToastContainer/>
+      <Navbar />
+      <hr />
+      <div className='app-content'>
+        <Sidebar />
+        <div className='main-content'>
+          <Routes>
+            <Route path='/' element={<Navigate to='/add' />} />
+            <Route path='/add' element={<ADD />} />
+            <Route path='/list' element={<List />} />
+            <Route path='/orders' element={<Order />} />
+          </Routes>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
